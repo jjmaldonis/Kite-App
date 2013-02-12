5aa0de17b85c55f993183530ef596ea379bc29d7
JASON MALDONIS <jjmaldonis@SMLBMACMINI4.local>
2013-02-12 14:47:10 -0600

    deleted the default detail view stuff. fixed it so that now the master view controller in the storyboard is displayed when i run the app. I had the MVC set to static cells, which I wanted, but then I had to get rid of the functions: numberOfSectionsInTableView:, numberOfRowsInSection:, and cellForRowAtIndexPath: to get the cells to actually show up.

--- Kiting/Kiting/KiteSpotMasterViewController.m
+++ Kiting/Kiting/KiteSpotMasterViewController.m
@@ -36,26 +36,10 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    NSLog(@"cellForRowAtIndexPath");
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Location" forIndexPath:indexPath];
-
-    /*NSLog(@"%@",_objects);
-    NSDate *object = _objects[indexPath.row];
-    cell.textLabel.text = [object description];*/
-    return cell;
-}
-
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
     // Return NO if you do not want the specified item to be editable.
     return NO;
-}
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
